700a12a96e81a2735f5bdf0197c4aaf16500bda0
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-21 03:32:56 +0500

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  productImages: string[] = [
+    'https://example.com/image1.jpg',
+    'https://example.com/image2.jpg',
+    'https://example.com/image3.jpg'
+
+  ];
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true
+  };
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.productImages
+  }
 }
