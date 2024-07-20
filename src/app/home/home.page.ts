0aaa523f60ddec7aad2b7cb34131f5e8ac2b5bc9
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-21 03:57:08 +0500

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,51 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides } from '@ionic/angular';;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
-
-  productImages: string[] = [
-    'https://example.com/image1.jpg',
-    'https://example.com/image2.jpg',
-    'https://example.com/image3.jpg'
-
-  ];
-
-  slideOpts = {
-    initialSlide: 0,
-    speed: 400,
-    autoplay: {
-      delay: 3000,
-    },
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    effect: 'fade',
-    slidesPerView: 1,
-    spaceBetween: 10
-  };
-
-
+export class HomePage {
   constructor() { }
 
-  ngOnInit(): void {
-    this.productImages
-  }
-  slidePrev(slides: IonSlides) {
-    slides.slidePrev();
-  }
 
-  slideNext(slides: IonSlides) {
-    slides.slideNext();
-  }
 }
