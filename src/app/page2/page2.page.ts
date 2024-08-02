470787757b6d0680d45805f3a5e01dc66c57b45a
saadurrehamn <saadrehman264@gmail.com>
2024-08-02 11:12:18 +0500

    update

--- src/app/page2/page2.page.ts
+++ src/app/page2/page2.page.ts
@@ -9,7 +9,21 @@ export class Page2Page implements OnInit {
 
   constructor() { }
 
+  followbtn: string = 'Follow';
+  random : boolean = true;
+
+  followChange(){
+    if (this.followbtn == 'Follow'){
+    
+      this.followbtn = 'Unfollow'
+    }else{
+      this.followbtn = 'Follow'
+    }
+  }
+
   ngOnInit() {
+    this.random;
+    this.followbtn;
   }
 
 }
